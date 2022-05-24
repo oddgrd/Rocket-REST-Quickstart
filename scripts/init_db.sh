e89035f455a69ebfb6d2915aea618b23f5cfcf55
oddgrd <oddgrd@gmail.com>
2022-05-24 19:33:45 +0200

    Flesh out init_db script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,11 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then
+  echo >&2 "Error: psql is not installed."
+  exit 1
+fi
+
+# Check if a custom user has been set, otherwise default to 'postgres'
+DB_USER=${POSTGRES_USER:=postgres}
+# Check if a custom password has been set, otherwise default to 'password'
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+# Check if a custom database name has been set, otherwise default to 'rocket_pg_template'
+DB_NAME="${POSTGRES_DB:=rocket_pg_template}"
+# Check if a custom port has been set, otherwise default to '5432'
+DB_PORT="${POSTGRES_PORT:=5432}"
+
+# Run Docker, with --rm tag to clean up filesystem on exit
 docker run \
   --rm \
-  --detach \
-  --name postgres \
-  --env POSTGRES_USER=username \
-  --env POSTGRES_PASSWORD=password \
-  --publish 127.0.0.1:8001:5432 \
-  postgres
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -p "${DB_PORT}":5432 \
+  -d postgres
 
+# Keep pinging Postgres until it's ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
